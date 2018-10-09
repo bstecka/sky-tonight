b2527e6621374889b4b49d082c234ebda16a59ef
Barbara <bstecka@gmail.com>
2018-10-09 12:07:03 +0200

    Add basic CalendarView to CalendarFragment

--- app/src/main/java/com/example/barbara/skytonight/core/CalendarFragment.java
+++ app/src/main/java/com/example/barbara/skytonight/core/CalendarFragment.java
@@ -3,10 +3,15 @@ package com.example.barbara.skytonight.core;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.barbara.skytonight.R;
 
@@ -31,7 +36,17 @@ public class CalendarFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_calendar, container, false);
+        final View view = inflater.inflate(R.layout.fragment_calendar, container, false);
+        CalendarView calendarView = view.findViewById(R.id.calendarView);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                Context context = view.getContext();
+                Toast.makeText(context, R.string.blank_fragment_calendar, Toast.LENGTH_SHORT).show();
+            }
+        });
+        return view;
     }
 
     @Override
