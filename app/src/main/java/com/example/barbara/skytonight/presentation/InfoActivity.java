c43c9d3027d387e6a1e8da218030fc6a44d6296c
Barbara <bstecka@gmail.com>
2018-12-03 09:03:57 +0100

    Testing and Acknowledgements

--- app/src/main/java/com/example/barbara/skytonight/presentation/InfoActivity.java
+++ app/src/main/java/com/example/barbara/skytonight/presentation/InfoActivity.java
@@ -1,11 +1,18 @@
 package com.example.barbara.skytonight.presentation;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.barbara.skytonight.R;
+import com.example.barbara.skytonight.presentation.util.LocaleHelper;
 
 public class InfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(LocaleHelper.onAttach(newBase));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
